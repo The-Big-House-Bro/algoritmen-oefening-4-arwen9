457de6b7f16afb3d81a8dc030deaf49e5c380fdc
Arwen De Mangeleer <arwen.demangeleer@leerling.athenea.be>
2025-03-25 09:19:17 +0100

    Oef 4 thuis opdracht

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,20 @@
-def countTargetPairs():
+def countTargetPairs(nums, target):
+    n = len(nums)
+    count = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] < target:
+                count += 1
+
+    return count
+
+nums = [-1, 1, 2, 3, 1]
+target = 2
+print(countTargetPairs(nums, target))
+
+
+nums = [-6, 2, 5, -2, -7, -1, 3]
+target = -2
+print(countTargetPairs(nums, target))
+
